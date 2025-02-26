9baf98fb69b5f4f392edac9aff9a7e6a671a92a8
Venkatesh <99860692+venkateshsarapu@users.noreply.github.com>
2025-02-26 16:03:48 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-infra-task45"
+    bucket         = "tf-infra-taskk"
     key            = "task2/state.tfstate"
     region         = "us-east-1"
   }
